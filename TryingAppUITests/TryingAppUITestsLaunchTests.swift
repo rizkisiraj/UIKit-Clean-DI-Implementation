20921aefd49b1b7013f271f3793c7571f4484add
rizki siraj <jomirwa@gmail.com>
2026-01-27 13:07:11 +0700

    Initial Commit

---
+++ TryingAppUITests/TryingAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TryingAppUITestsLaunchTests.swift
+//  TryingAppUITests
+//
+//  Created by Rizki Siraj on 27/01/26.
+//
+
+import XCTest
+
+final class TryingAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
